75f179a508667f28cdff17f589a82b33195aa29b
Gavin Moy <gmoy@ucsc.edu>
2019-05-02 13:48:14 -0700

    what i think objcreated should look like

--- plugins/crd/handler/handler.go
+++ plugins/crd/handler/handler.go
@@ -1,62 +1,193 @@
-// Copyright (c) 2018 Cisco and/or its affiliates.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at:
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/*
+ * // Copyright (c) 2018 Cisco and/or its affiliates.
+ * //
+ * // Licensed under the Apache License, Version 2.0 (the "License");
+ * // you may not use this file except in compliance with the License.
+ * // You may obtain a copy of the License at:
+ * //
+ * //     http://www.apache.org/licenses/LICENSE-2.0
+ * //
+ * // Unless required by applicable law or agreed to in writing, software
+ * // distributed under the License is distributed on an "AS IS" BASIS,
+ * // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * // See the License for the specific language governing permissions and
+ * // limitations under the License.
+ */
 
-package handler
+//go:generate protoc -I ./model --gogo_out=plugins=grpc:./model ./model/bgpconfig.proto
+
+package bgpconfig
 
 import (
-	"github.com/contiv/vpp/plugins/crd/handler/nodeconfig"
-	"github.com/contiv/vpp/plugins/crd/handler/telemetry"
+	"github.com/contiv/contiv-vpp/plugins/crd/handler/bgpconfig/model"
+	"github.com/contiv/contiv-vpp/plugins/crd/pkg/apis/bgpconfig/v1"
+	"sync"
+
+	informers "github.com/contiv/contiv-vpp/plugins/crd/pkg/client/informers/externalversions/bgpconfig/v1"
+	"github.com/gogo/protobuf/proto"
+	"github.com/ligato/cn-infra/datasync"
+	"github.com/ligato/cn-infra/datasync/kvdbsync"
+	"github.com/ligato/cn-infra/db/keyval"
+	"github.com/ligato/cn-infra/logging"
 )
 
-// Handler is implemented by any handler.
-// The Handle method is used to process an event
-type Handler interface {
-	//
-	Init() error
-	//
-	ObjectCreated(obj interface{})
-	//
-	ObjectDeleted(obj interface{})
-	//
-	ObjectUpdated(oldObj, newObj interface{})
+const (
+	minResyncTimeout = 100  // minimum timeout between resync attempts, in ms
+	maxResyncTimeout = 1000 // maximum timeout between resync attempts, in ms
+)
+
+// Handler handler implements Handler interface,
+type Handler struct {
+	Deps
+
+	// Data store sync status and the mutex that protects access to it
+	dsSynced bool
+	dsMutex  sync.Mutex
+
+	broker KeyProtoValBroker
+	prefix string
+	kpc    K8sToProtoConverter
+
+	syncStopCh chan bool
 }
 
-// Map maps each event handler function to a name for easily lookup
-var Map = map[string]interface{}{
-	"default":    &Default{},
-	"telemetry":  &telemetry.Handler{},
-	"nodeConfig": &nodeconfig.Handler{},
+// Deps defines dependencies for BgpConfig CRD Handler.
+type Deps struct {
+	Log                logging.Logger
+	Publish            *kvdbsync.Plugin // KeyProtoValWriter does not define Delete
+	ControllerInformer informers.BgpConfigInformer
 }
 
-// Default handler implements Handler interface
-type Default struct {
+// KeyProtoValBroker defines handler's interface to the key-value data store. It
+// defines a subset of operations from a generic cn-infra broker interface
+// (keyval.ProtoBroker in cn-infra).
+type KeyProtoValBroker interface {
+	// Put <data> to ETCD or to any other key-value based data source.
+	Put(key string, data proto.Message, opts ...datasync.PutOption) error
+
+	// Delete data under the <key> in ETCD or in any other key-value based data
+	// source.
+	Delete(key string, opts ...datasync.DelOption) (existed bool, err error)
+
+	// GetValue reads a value from etcd stored under the given key.
+	GetValue(key string, reqObj proto.Message) (found bool, revision int64, err error)
+
+	// List values stored in etcd under the given prefix.
+	ListValues(prefix string) (keyval.ProtoKeyValIterator, error)
 }
+
+// K8sToProtoConverter defines the signature for a function converting k8s
+// objects to bgp config protobuf objects.
+type K8sToProtoConverter func(interface{}) (interface{}, string, bool)
+
+// DsItems defines the structure holding items listed from the data store.
+type DsItems map[string]interface{}
+
+
+
+
 
 // Init initializes handler configuration
-// Do nothing for default handler
-func (d *Default) Init() error {
+// BgpConfig Handler will be taking action on resource CRUD
+func (h *Handler) Init() error {
+
+	ksrPrefix := h.Publish.ServiceLabel.GetAgentPrefix()
+	h.broker = h.Publish.Deps.KvPlugin.NewBroker(ksrPrefix)
+	h.syncStopCh = make(chan bool, 1)
+	h.prefix = "/vnf-agent/vpp1/config/bgp/v1/"
+
+	h.kpc = func(obj interface{}) (interface{}, string, bool) {
+		bgpConfig, ok := obj.(*v1.BgpConfig)
+		if !ok {
+			h.Log.Warn("Failed to cast newly created node-config object")
+			return nil, "", false
+		}
+		return h.bgpConfigToProto(bgpConfig), "global", true
+	}
+
 	return nil
 }
 
 // ObjectCreated is called when a CRD object is created
-func (d *Default) ObjectCreated(obj interface{}) {
+func (h *Handler) ObjectCreated(obj interface{}) {
+	h.Log.Debugf("Object created with value: %v", obj)
+	bgpConfig, ok := obj.(*v1.BgpConfig)
+	if !ok {
+		h.Log.Warn("Failed to cast newly created bgp-config object")
+		return
+	}
+	globalConfigProto := h.bgpGlobalConfigToProto(bgpConfig.Spec.BGPGlobal)
+	err := h.Publish.Put("global", globalConfigProto)
+	h.Log.Errorf("error publish.put global : %v" , err)
+	for _, nextPeer := range bgpConfig.Spec.Peers {
+		peerProto := h.bgpPeersConfigToProto(nextPeer)
+		err := h.Publish.Put("peers/" + nextPeer.Name, peerProto)
+		h.Log.Errorf("error publish.put peer : %v" , err)
+	}
+	/*
+	if err != nil {
+		h.dsSynced = false
+		h.startDataStoreResync()
+	}
+	*/
+
 }
 
 // ObjectDeleted is called when a CRD object is deleted
-func (d *Default) ObjectDeleted(obj interface{}) {
+func (h *Handler) ObjectDeleted(obj interface{}) {
+	h.Log.Debugf("Object deleted with value: %v", obj)
+
 }
 
 // ObjectUpdated is called when a CRD object is updated
-func (d *Default) ObjectUpdated(oldObj, newObj interface{}) {
+func (h *Handler) ObjectUpdated(oldObj, newObj interface{}) {
+	h.Log.Debugf("Object updated with value: %v", newObj)
+
+}
+
+// bgpConfigToProto converts bgp-config data from the Contiv's own CRD representation
+// into the corresponding protobuf-modelled data format.
+func (h *Handler) bgpConfigToProto(bgpConfig *v1.BgpConfig) *model.BgpConf {
+	bgpConfigProto := &model.BgpConf{}
+	bgpConfigProto.Global = h.bgpGlobalConfigToProto(v1.GlobalConf{})
+
+	for _, nextPeer := range bgpConfig.Spec.Peers {
+		bgpConfigProto.Peers = append(bgpConfigProto.Peers,
+			h.bgpPeersConfigToProto(nextPeer))
+	}
+
+	return bgpConfigProto
+}
+
+func (h *Handler) bgpPeersConfigToProto(bgpPeersConfig v1.PeerConf) *model.PeerConf {
+	bgpPeersConfigProto := &model.PeerConf{}
+	bgpPeersConfigProto.Name = bgpPeersConfig.Name
+	bgpPeersConfigProto.AuthPassword = bgpPeersConfig.AuthPassword
+	bgpPeersConfigProto.Description = bgpPeersConfig.Description
+	bgpPeersConfigProto.LocalAs = bgpPeersConfig.LocalAs
+	bgpPeersConfigProto.NeighborAddress = bgpPeersConfig.NeighborAddress
+	bgpPeersConfigProto.PeerAs = bgpPeersConfig.PeerAs
+	bgpPeersConfigProto.PeerGroup = bgpPeersConfig.PeerGroup
+	bgpPeersConfigProto.PeerType = bgpPeersConfig.PeerType
+	//bgpPeersConfigProto.RemovePrivateAs = bgpPeersConfig.RemovePrivateAs
+	bgpPeersConfigProto.RouteFlapDamping = bgpPeersConfig.RouteFlapDamping
+	bgpPeersConfigProto.SendCommunity = bgpPeersConfig.SendCommunity
+	bgpPeersConfigProto.NeighborInterface = bgpPeersConfig.NeighborInterface
+	bgpPeersConfigProto.Vrf = bgpPeersConfig.Vrf
+	bgpPeersConfigProto.AllowOwnAs = bgpPeersConfig.AllowOwnAs
+	bgpPeersConfigProto.ReplacePeerAs = bgpPeersConfig.ReplacePeerAs
+	bgpPeersConfigProto.AdminDown = bgpPeersConfig.AdminDown
+
+	return bgpPeersConfigProto
+}
+
+func (h *Handler) bgpGlobalConfigToProto(bgpGlobalConfig v1.GlobalConf) *model.GlobalConf {
+	bgpGlobalConfigProto := &model.GlobalConf{}
+	bgpGlobalConfigProto.As = bgpGlobalConfig.As
+	bgpGlobalConfigProto.Families = bgpGlobalConfig.Families
+	bgpGlobalConfigProto.ListenAddresses = bgpGlobalConfig.ListenAddresses
+	bgpGlobalConfigProto.RouterId = bgpGlobalConfig.RouterId
+	bgpGlobalConfigProto.UseMultiplePaths = bgpGlobalConfig.UseMultiplePaths
+
+	return bgpGlobalConfigProto
 }
